dc6820d015d11e6c8c8f1d996cfeb3d6144181d8
Pietari Jukarainen <jappappi@dhcp-asv-90.helsinki.fi>
2016-04-04 20:43:42 +0300

    tietokantatesti

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,4 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Player (handle, name, sponsor, country, characters, tournaments, points, description)
+VALUES ('Sikabonzai', 'Pete Jarmonen', 'Pena', 'Finland', '{Zangief, R Mika}', '{Kallio FirstStrike}', 0, 'So nice and guy yes but no');
+
+INSERT INTO Tournament VALUES ('Jarmon katutappelu', '03-18-2016 - 03-21-2016', 'Helsinki', 'Premier', 'Europe', 'Helsingin kovimmat ottaa mittaa', '{Kalervo, Jarmo, Pertti, Juuso}');
